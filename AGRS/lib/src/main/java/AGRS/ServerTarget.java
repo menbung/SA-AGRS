5d5725629bdee55ba26c14ba1f54991611872e94
HS314 <khss314@konkuk.ac.kr>
2021-12-18 06:34:03 +0900

    Revert "Revert "Connect map""
    This reverts commit 3d43f7adc800a170e95efa849d53f1d6fdfc8dd4.

--- AGRS/lib/src/main/java/AGRS/ServerTarget.java
+++ AGRS/lib/src/main/java/AGRS/ServerTarget.java
@@ -4,5 +4,5 @@ import java.util.ArrayList;
 
 public interface ServerTarget {
 	public ArrayList<FlightInfoObject> flightInfoReq(String date);
-	public ArrayList<ArrayList<Integer>> mapReq();
+	public int[] mapReq();
 }
