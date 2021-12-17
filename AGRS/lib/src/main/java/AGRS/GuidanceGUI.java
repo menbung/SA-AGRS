04fb3306b586a2c8f7ab529f7934b355237c61ae
HS314 <khss314@konkuk.ac.kr>
2021-12-18 06:13:00 +0900

    guidancegui

--- AGRS/lib/src/main/java/AGRS/GuidanceGUI.java
+++ AGRS/lib/src/main/java/AGRS/GuidanceGUI.java
@@ -1,17 +1,23 @@
 package AGRS;
 
 import java.awt.*;
+import java.util.Vector;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GuidanceGUI extends JFrame {
 
 	private JPanel contentPane;
 	private JTable tableMap;
 	private String msg;
+	private Vector<JPanel> RouteList;
+	private JLayeredPane layeredPane;
 	
 	/**
-	 * Launch the application.
+	 * 테스트용
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -58,36 +64,130 @@ public class GuidanceGUI extends JFrame {
 		
 		
 		
-		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane = new JLayeredPane();
 		contentPane.add(layeredPane, BorderLayout.CENTER);
 		layeredPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("G");
-		btnNewButton.setBackground(Color.GREEN);
-		btnNewButton.setBounds(-12, -12, 48, 48);
-		layeredPane.add(btnNewButton);
+		JButton Goal = new JButton("G");
+		Goal.setEnabled(false);
+		Goal.setForeground(Color.BLACK);
+		layeredPane.setLayer(Goal, 100);
+		Goal.setFont(new Font("굴림", Font.BOLD, 14));
+		Goal.setBackground(Color.GREEN);
+		Goal.setBounds(-12, -12, 48, 48);
+		layeredPane.add(Goal);
 		
 		
 		tableMap = new JTable(6,12);
+		tableMap.setEnabled(false);
+		tableMap.setRowSelectionAllowed(false);
 		tableMap.setBounds(12, 12, 912, 432);
 		tableMap.setRowHeight(72);
 		layeredPane.add(tableMap);
 		
 		JButton Robot = new JButton("R");
+		
+		// 테스트용
+		Robot.addMouseListener(new MouseAdapter() {		//임시 테스트용 R 클릭했을때 리페인트 되는지 확인용.
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Point p1 = new Point();
+				p1.setPoint(3, 4);
+				Point p2 = new Point();
+				p2.setPoint(3, 3);
+				makeRouteY(p1, p2);
+				System.out.println(RouteList.get(0).getX());
+				printRoute();
+				
+			}
+		});
 		Robot.setFont(new Font("굴림", Font.BOLD, 14));
-		layeredPane.setLayer(Robot, 1);
+		layeredPane.setLayer(Robot, 101);
 		Robot.setBackground(Color.RED);
 		Robot.setBounds(672,204,48,48);
 		layeredPane.add(Robot);
 		
+//		JPanel RouteX = new JPanel();	// 크기 테스트용
+//		layeredPane.setLayer(RouteX, 1);
+//		RouteX.setBackground(Color.RED);
+//		RouteX.setBounds(236, 152, 84, 8);
+//		layeredPane.add(RouteX);
+//		
+//		JPanel RouteY = new JPanel();
+//		RouteY.setBackground(Color.RED);
+//		layeredPane.setLayer(RouteY, 1);
+//		RouteY.setBounds(312, 152, 8, 80);
+//		layeredPane.add(RouteY);
+		
 		//길찾기 시뮬레이션부
+		RouteList = new Vector<JPanel>();
 		
 		
 		
 	}
-	class tableMap extends JPanel{
-		public void paintComponent(Graphics g){
-			g.drawOval(120, 70, 60, 60);
+	
+//	public void makeRoute() {	// 이건 핸들러에서 해야할것 같아서 임시로 주석처리.
+//		if() {
+//			
+//		}else if(){
+//			
+//		}else if() {
+//			
+//		}else if(){
+//			
+//		}else {
+//			
+//		}
+//	}
+	
+	
+	public void makeRouteX(Point previous, Point current){	//x축 루트 막대기 생성용
+		if(previous.getX()<current.getX()) {
+			JPanel RouteX = new JPanel();
+			RouteX.setBackground(Color.RED);
+			RouteX.setBounds(76*previous.getX()+8, 72*previous.getY()+8, 84, 8);
+			RouteList.add(RouteX);
+		}else if(previous.getX()>current.getX()){
+			JPanel RouteX = new JPanel();
+			RouteX.setBackground(Color.RED);
+			RouteX.setBounds(76*current.getX()+8, 72*current.getY()+8, 84, 8);
+			RouteList.add(RouteX);
+			
+		}else {
+			System.out.println("error in makeRoute x");
 		}
 	}
+	
+	
+	
+	public void makeRouteY(Point previous, Point current) {	//y축 루트 막대기 생성용
+		if(previous.getY()<current.getY()) {
+			JPanel RouteY = new JPanel();
+			RouteY.setBackground(Color.RED);
+			RouteY.setBounds(76*previous.getY()+8, 72*previous.getY()+8, 8, 84);
+			RouteList.add(RouteY);
+		}else if(previous.getY()>current.getY()){
+			JPanel RouteY = new JPanel();
+			RouteY.setBackground(Color.RED);
+			RouteY.setBounds(76*current.getY()+8, 72*current.getY()+8, 8, 84);
+			RouteList.add(RouteY);
+			
+		}else {
+			System.out.println("error in makeRoute y");
+		}
+	}
+	
+	
+	public void printRoute() {	//루트 all print
+		for(int i=0; i<RouteList.size();i++) {
+			layeredPane.setLayer(RouteList.elementAt(i), 10);
+			contentPane.revalidate();
+			contentPane.repaint();
+//			layeredPane.revalidate();
+//			layeredPane.repaint();
+		}
+	}
+
+	
+	
 }
